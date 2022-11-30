5b47a4cc96babb3748f821e6fa83ad2e7dab2c23
Ravi Pratap Singh <ravip0451@gmail.com>
2022-11-30 11:32:14 +0530

    RECO SYSTEM

---
+++ app.py
@@ -0,0 +1,144 @@
+
+import streamlit as st
+import json
+from Classifier import KNearestNeighbours
+from operator import itemgetter
+
+from PIL import Image
+from streamlit_option_menu import option_menu
+import requests
+from streamlit_lottie import st_lottie
+import streamlit.components.v1 as components
+
+
+
+#Setting the Page Configuration
+#img = Image.open('./images/favicon.png')
+st.set_page_config(page_title='Movie Recommender Engine' ,  layout="centered",initial_sidebar_state="expanded")
+
+    
+
+
+#Loading the animation of the streamlit lottie
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+# URLS of all the lottie animation used
+lottie_coding = load_lottieurl("https://assets9.lottiefiles.com/private_files/lf30_bb9bkg1h.json")
+lottie_contact =load_lottieurl("https://assets2.lottiefiles.com/packages/lf20_dhcsd5b5.json")
+lottie_loadLine =load_lottieurl("https://assets6.lottiefiles.com/packages/lf20_yyjaansa.json")
+lottie_github =load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_S6vWEd.json")
+
+
+
+
+
+
+# Loading data and movies list from corresponding JSON files
+with open(r'data.json', 'r+', encoding='utf-8') as f:
+    data = json.load(f)
+with open(r'titles.json', 'r+', encoding='utf-8') as f:
+    movie_titles = json.load(f)
+
+
+#Applying the KNN algorithms on to the point
+def knn(test_point, k):
+    # Create dummy target variable for the KNN Classifier
+    target = [0 for item in movie_titles]
+
+    # Instantiate object for the Classifier
+    model = KNearestNeighbours(data, target, test_point, k=k)
+
+    # Run the algorithm
+    model.fit()
+    # Distances to most distant movie
+    max_dist = sorted(model.distances, key=itemgetter(0))[-1]
+    # Print list of 10 recommendations < Change value of k for a different number >
+    table = list()
+    for i in model.indices:
+        # Returns back ---> Movie title and IMDB link
+        table.append([movie_titles[i][0], movie_titles[i][2]])
+    return table
+
+#All the genres from which a user can select
+if __name__ == '__main__':
+    genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family',
+              'Fantasy', 'Film-Noir', 'Game-Show', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News',
+              'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
+    
+    movies = [title[0] for title in movie_titles]
+    
+    #Designing of the header and main section of the application.
+    with st.container():
+     left_column, right_column = st.columns(2)
+     with left_column:
+            st.write("")
+            st.title('MOVIE RECOMMENDER ENGINE') 
+     with right_column:
+            st_lottie(lottie_coding, height=300,width=400, key="coding")
+        
+    
+    #Selection basis of recommendation.
+
+    apps = ['*--Select--*', 'Movie based', 'Genres based']   
+    app_options = st.selectbox('Method Of Recommendation:', apps)
+
+
+    
+    #If Movie Based Recommendation is being selected this condtion will get executed.
+    if app_options == 'Movie based':
+        movie_select = st.selectbox('Select a movie:', ['--Select--'] + movies)
+        if movie_select == '--Select--':
+            st.write('Select a movie')
+        else:
+            n = st.number_input('Number of movies:', min_value=5, max_value=20, step=1)
+            genres = data[movies.index(movie_select)]
+            test_point = genres
+            table = knn(test_point, n)
+            st.write("")
+            st.write("")
+            st. markdown("<h1 style='text-align: center; color:#A0CFD3;'> RECOMMENDED MOVIES 📈 </h1>", unsafe_allow_html=True)
+            st.write("")
+            st.write("")
+            
+            for movie, link in table:
+                st.warning(movie)
+                st.markdown(f"📌 IMDB LINK --- [{movie}]({link})")
+
+        
+    #If Genre Based Recommendation is being selected this condtion will get executed.
+    elif app_options == apps[2]:
+        options = st.multiselect('Select genres:', genres)
+        if options:
+            imdb_score = st.slider('IMDb score:', 1, 10, 8)
+            n = st.number_input('Number of movies:', min_value=5, max_value=20, step=1)
+            
+            test_point = [1 if genre in options else 0 for genre in genres]
+            test_point.append(imdb_score)
+            table = knn(test_point, n)
+            st.write("")
+            st.write("")
+            st. markdown("<h1 style='text-align: center; color:#A0CFD3;'> RECOMMENDED MOVIES 📈 </h1>", unsafe_allow_html=True)
+            st.write("")
+            st.write("")
+            
+            for movie, link in table:
+                # Displays movie title with link to imdb
+                st.warning(movie)
+                st.markdown(f"📌 IMDB LINK --- [{movie}]({link})")
+
+        else:
+                st.write(" _Can Select Multiple Genres_ ")
+                        
+
+    else:
+        st.write('Select option')
+
+
+
+
+       
